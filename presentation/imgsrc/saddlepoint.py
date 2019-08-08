72832c1bfd1066932ab682762a4afed959cf3349
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2019-08-08 16:09:20 +0200

    code style improved

--- presentation/imgsrc/saddlepoint.py
+++ presentation/imgsrc/saddlepoint.py
@@ -19,53 +19,57 @@ text.preamble(r'''\usepackage[sfdefault,lining,scaled=.85]{FiraSans}
 unit.set(vscale=1.2, wscale=1.3, xscale=1.3)
 
 c = canvas.canvas()
-p = painter.regular(basepathattrs=[deco.earrow], titlepos=1,
-                    outerticklength=painter.ticklength.normal,
-                    titledist=-0.35, titleattrs=[text.halign.right], titledirection=None) # horizontal text
 
-p1y = painter.regular(basepathattrs=[deco.earrow], titlepos=1.03,
-                    outerticklength=painter.ticklength.normal,
-                    titledist=0.15, titleattrs=[text.valign.top], titledirection=None) # horizontal text
+painter_x = painter.regular(basepathattrs=[deco.earrow],
+                            titlepos=1, titledist=-0.35,
+                            titleattrs=[text.halign.right])
+
+painter_y = painter.regular(basepathattrs=[deco.earrow],
+                            titlepos=1.03, titledist=0.15,
+                            titleattrs=[text.valign.top], titledirection=None)
 
 g1 = c.insert(
          graphxy(width=5, height=2, xaxisat=0, yaxisat=0,
-             x=axis.linear(title="$x$", min=0, max=xrange,
-                           painter=p, parter=None),
-             y=axis.linear(title=r"$I(x)$", max=1.2, painter=p1y, parter=None)
-         )
+                 x=axis.linear(title="$x$", min=0, max=xrange,
+                               painter=painter_x, parter=None),
+                 y=axis.linear(title=r"$I(x)$", max=1.2,
+                               painter=painter_y, parter=None)
+                 )
      )
 
 x2 = np.linspace(0, (0.93*xrange)**2, 800)
 factor = sqrt(0.5*pi)
 _, integral = fresnel(np.sqrt(x2)/factor)
 integral = factor*integral
-g1.plot(data.points(list(zip(np.sqrt(x2), integral)), x=1, y=2), lineproperties)
+g1.plot(data.points(list(zip(np.sqrt(x2), integral)), x=1, y=2),
+        lineproperties)
 
 g2 = c.insert(
          graphxy(xpos=g1.xpos-g1.width, ypos=g1.ypos+g1.height+0.5,
-             width=10, height=2, xaxisat=0, yaxisat=0,
-             x=axis.linear(title="$x$", min=-xrange, max=xrange, painter=p,
-                           parter=None),
-             y=axis.linear(title=r"$f(x)$", min=-1.1, max=2, painter=p1y,
-                           parter=None)
-         )
+                 width=10, height=2, xaxisat=0, yaxisat=0,
+                 x=axis.linear(title="$x$", min=-xrange, max=xrange,
+                               painter=painter_x, parter=None),
+                 y=axis.linear(title=r"$f(x)$", min=-1.1, max=2,
+                               painter=painter_y, parter=None)
+                 )
      )
 
 phase = np.cos(x2)
 for sign in (-1, 1):
-    g2.plot(data.points(list(zip(sign*np.sqrt(x2), phase)), x=1, y=2), lineproperties)
+    g2.plot(data.points(list(zip(sign*np.sqrt(x2), phase)), x=1, y=2),
+            lineproperties)
 
 g3 = c.insert(
          graphxy(xpos=g2.xpos, ypos=g2.ypos+g2.height+0.5,
-             width=10, height=2, xaxisat=0, yaxisat=0,
-             x=axis.linear(title="$x$", min=-xrange, max=xrange, painter=p,
-                           parter=None),
-             y=axis.linear(title=r"$S(x)$", painter=p1y, parter=None)
-         )
+                 width=10, height=2, xaxisat=0, yaxisat=0,
+                 x=axis.linear(title="$x$", min=-xrange, max=xrange,
+                               painter=painter_x, parter=None),
+                 y=axis.linear(title=r"$S(x)$", painter=painter_y, parter=None)
+                 )
      )
 
 x = np.linspace(-0.95*xrange, 0.95*xrange, 200)
 action = x**2
 g3.plot(data.points(list(zip(x, action)), x=1, y=2), lineproperties)
- 
+
 c.writePDFfile()
